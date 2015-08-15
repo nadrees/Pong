6e57f661b597949a7e35424f26c17a0eff5c19dc
Nathen Drees <nadrees@gmail.com>
2015-08-15 14:37:41 -0500

    Add ball bouncing off walls

---
+++ Pong/Assets/Scripts/BallController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour {
+	public float Speed;
+
+	Rigidbody rigidBody;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+
+		var lrStartingDirection = Random.value < .5 ? -1 : 1;
+		var udStartingDirection = 1; //Random.value * 2 - 1;
+		rigidBody.velocity = (new Vector3(lrStartingDirection, udStartingDirection) * Speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Wall"))
+			rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y * -1);
+	}
+}
